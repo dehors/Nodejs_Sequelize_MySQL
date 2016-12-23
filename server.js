4ef09f9819ab99a0ae855771327c11490e173198
slam24 <luismartinez242011@gmail.com>
2016-12-23 12:27:10 -0600

    Import models

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// BASE SETUP
+// =============================================================================
 var express = require('express'),
 	bodyParser = require('body-parser');
 
@@ -6,3 +8,28 @@ app.use(bodyParser());
 
 var env = app.get('env') == 'development' ? 'dev' : app.get('env');
 var port = process.env.PORT || 8080;
+
+var Sequelize = require('sequelize');
+
+// IMPORT MODELS
+// =============================================================================
+// db config
+var env = "dev";
+var config = require('./database.json')[env];
+var password = config.password ? config.password : null;
+
+// initialize database connection
+var sequelize = new Sequelize(
+	config.database,
+	config.user,
+	config.password,
+	{
+		logging: console.log,
+		define: {
+			timestamps: false
+		}
+	}
+);
+
+var crypto = require('crypto');
+var DataTypes = require("sequelize");
